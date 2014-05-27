4b48a0b2fab545bbd3a818facc78e41a16b93200
DonLeung <new@DonLeung.local>
2014-05-27 12:30:09 -0700

    modified insertionSort

--- sorting/insertionSort.py
+++ sorting/insertionSort.py
@@ -1,21 +1,17 @@
-'''
-Created on May 26, 2014
 
-@author: Don Liang
-'''
 
 def insertionSort(alist):
     pivot = alist[0]
-    i = 0
-    for index in range( 1, len(alist)):
-        while i <= index:
-            if pivot > alist[index]:
-                temp = pivot
-                alist[index-1] = alist[index]
-                alist[index] = temp
+    i = 1    
+    for index in range( 1, len(alist) ):  # insert a number to the list
+        while i <= index:                 # comparisons in a certain index
+            if alist[i-1] > alist[index]:     # switch element
+                temp = alist[index]
+                alist[index] = alist[i-1]
+                alist[i-1] = temp
+                # print (i) / print (alist) will showing the process of insertion sort
             i += 1
-        i = 0
-        pivot = alist[0]
+        i = 1
     return alist
 
 alist = [54,26,93,17,77,31,44,55,20]
