b59200da5886177d511f393c778aeeb4f9f493c2
Don Liang <duolingl@gmail.com>
2014-05-31 14:23:13 -0700

    Node.class update

--- data_structure/Linkedlist_Node.py
+++ data_structure/Linkedlist_Node.py
@@ -2,4 +2,16 @@ class Node:
 	def __init__ (self,initdata):
 		self.data = initdata
 		self.next = None
-	def 
+	def getData (self):
+		return self.data
+	def getNext (self):
+		return self.next
+	def addData(self, addedData):
+		self.data = addedData
+	def addNext(self, nextNode):
+		self.next = nextNode
+
+temp = Node("hello")
+print(temp.getData())
+temp.addData(98)
+print(temp.getData())
