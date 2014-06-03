09a60eb1ebb1a4e5a475d08a579351733be7fa09
Don Liang <duolingl@gmail.com>
2014-06-02 23:32:39 -0700

    add linked_list folder

---
+++ linked_list/mergeLinkedList.py
@@ -0,0 +1,10 @@
+'''
+Created on Jun 2, 2014
+
+@author: new
+'''
+# my idea is to connect the shorter linked list to the tail of 
+# the first linked list, then two pointers to point two heads 
+# separately and swap the value.
+
+# will update code tomorrow
